8deadd7bb0d397babe8b4956745a088d4ab1cc0f
Jeronimo Barea Lucas <jeronimobarealucas@gmail.com>
2023-07-08 11:05:08 +0200

    feat: minor style changes

--- toml-merge.ml
+++ toml-merge.ml
@@ -12,10 +12,10 @@ let list_files_by_extension dir ext =
     Sys.readdir dir
     |> Array.to_list
     |> List.filter (fun f -> Filename.extension f = ext)
-    |> List.map (fun f -> Printf.sprintf "%s/%s" dir f)
+    |> List.map (fun f -> Printf.sprintf "%s%s" dir f)
 
 let read_file f =
-  Printf.printf "reading file: %s\n" f;
+  Printf.printf "reading file: %s" f; print_newline ();
   let ic = open_in f in
   let content = really_input_string ic (in_channel_length ic) in
   close_in ic;
@@ -25,7 +25,7 @@ let create_file_with_extension content dir name ext =
   let f = Printf.sprintf "%s%s%s" dir name ext in
   let oc = open_out f in
   output_string oc content;
-  Printf.printf "saving into file: %s\n" f;
+  Printf.printf "saving into file: %s" f; print_newline ();
   close_out oc
 
 let merge dir_in dir_out filename =
@@ -61,4 +61,4 @@ let () =
   if !output_file = "" then
     output_file := "config";
 
-  merge !dir_in !dir_out !output_file;
+  merge !dir_in !dir_out !output_file
